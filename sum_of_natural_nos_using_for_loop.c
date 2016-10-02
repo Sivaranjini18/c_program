d99924d86f60d9e383ff0ab106af57c08960f0c0
Sivaranjini18 <ranjini.krishnan18081995@gmail.com>
2016-10-02 13:43:02 +0530

    Add files via upload

---
+++ sum_of_natural_nos_using_for_loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n,i,sum=0;
+printf("Enter n value");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("The sum of first %d natural no. is %d",n,sum);
+}
